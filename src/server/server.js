735e12c481b538cdb0c319eab3fe86f1949399f9
jay imbery <jayimbery@hotmail.com>
2022-08-14 22:49:35 +0100

    adding +1 voting system and general improvements

--- src/server/server.js
+++ src/server/server.js
@@ -3,57 +3,93 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client({
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const clientCredentials = {
   user: 'postgres',
   host: 'localhost',
   database: 'dubvsubdb',
   password: 'brighton1',
   port: 5432,
-});
-
-client.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to DB!');
-});
+};
 
 const updateDB = async (animeData) => {
+  const pool = new Pool(clientCredentials);
   const animeDataJson = JSON.parse(animeData);
   const animeTitle = animeDataJson.title;
   const animeId = animeDataJson.id;
   const animeSynopsis = animeDataJson.synopsis;
   const animeYear = animeDataJson.year;
   const imgUrl = animeDataJson.imageUrl;
-  await client.query(
-    'INSERT INTO rating_page(TITLE, ID, SYNOPSIS, YEAR, IMG_URL) VALUES($1, $2, $3, $4, $5) RETURNING *',
-    [animeTitle, animeId, animeSynopsis, animeYear, imgUrl],
-    (err, res) => {
-      console.log(err, res);
-    },
-  );
+  let output;
+  await pool.query(
+    'INSERT INTO rating_page(TITLE, ID, SYNOPSIS, YEAR, IMG_URL, DUB_RATING, SUB_RATING) VALUES($1, $2, $3, $4, $5, 0, 0) RETURNING *',
+    [animeTitle, animeId, animeSynopsis, animeYear, imgUrl])
+    .then((res) => {
+      output = res;
+      console.log(output);
+    });
+    
+  await pool.end();
+  return output;
 };
 
-const checkDB = (animeData) => {
+const checkDB = async (animeData) => {
+  const pool = new Pool(clientCredentials);
   const animeId = JSON.parse(animeData).id;
-  client.query(
+  // let entryExists = 'doot';
+  let output;
+  await pool.query('SELECT * FROM rating_page WHERE ID = $1', [animeId])
+    .then(async (result) => {
+      if (result.rows.length === 0) {
+        output = await updateDB(animeData);
+      } else {
+        output = result;
+      }
+      console.log(output);
+    });
+  await pool.end();
+  return output;
+};
+
+const SQLCheckDB = (animeId, animeData) => {
+  const pool = new Pool(clientCredentials);
+  let output;
+  let entryExists;
+  pool.query(
     'SELECT * FROM rating_page WHERE ID = $1',
     [animeId],
     (err, result) => {
       if (result.rows.length === 0) {
+        entryExists = 'false';
+        // console.log(entryExists);
         updateDB(animeData);
+      } else {
+        entryExists = 'true';
+        output = result;
       }
+
+      // console.log(result);
+      // console.log(output);
     },
   );
+  console.log(output);
+  pool.end();
+  return (output, entryExists);
 };
 
 // Search
 app.get('/search', async (req, res) => {
   const usedData = [];
   const search = req.query.q;
+  let dbData;
   await axios
     .get(`https://api.jikan.moe/v4/anime?q=${search}&limit=10`)
-    .then((result) => {
+    .then(async (result) => {
       const listOfAnimes = result.data.data;
 
       for (let i = 0; i < listOfAnimes.length; i += 1) {
@@ -64,49 +100,79 @@ app.get('/search', async (req, res) => {
         } else {
           animeTitle = data.title;
         }
+
         usedData.push(
           `{ "title": "${animeTitle}", "id": "${data.mal_id}", "synopsis": "${data.synopsis}", "imageUrl": "${data.images.jpg.image_url}"}`,
         );
+
         console.log(usedData);
+        console.log(dbData);
       }
     })
     .catch((error) => {
       throw error;
     });
-  res.send(usedData);
+  res.send(dbData);
 });
 
 // get anime data
 app.get('/anime', async (req, res) => {
   const animeId = req.query.id;
   const animeData = [];
+  let dbData;
 
   await axios
     .get(`https://api.jikan.moe/v4/anime/${animeId}`)
-    .then((result) => {
+    .then(async (result) => {
       const { data } = result.data;
       let animeTitle = '';
+      const animeSynopsis = data.synopsis.replace(/(["])/g, '\\$1');
 
       if (data.title_english !== null) {
         animeTitle = data.title_english;
       } else {
         animeTitle = data.title;
       }
+
       animeData.push(
-        `{ "title": "${animeTitle}", "id": "${data.mal_id}", "synopsis": "${data.synopsis}", "imageUrl": "${data.images.jpg.image_url}", "year": ${data.year}}`,
+        `{ "title": "${animeTitle}", "id": "${data.mal_id}", "synopsis": "${animeSynopsis}", "imageUrl": "${data.images.jpg.image_url}", "year": ${data.year}}`,
       );
-      console.log(JSON.parse(animeData));
-      checkDB(animeData);
+      dbData = await checkDB(animeData);
+      // console.log(dbData);
     })
     .catch((error) => {
       throw error;
     });
 
-  res.send(JSON.parse(animeData));
+  await res.send(dbData);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// voting system
+const addVote = async (vote, id) => {
+  const pool = new Pool(clientCredentials);
+  let queryString;
+  let result;
+  if (vote === 'dub') {
+    queryString = 'UPDATE rating_page SET dub_rating = dub_rating + 1 WHERE id = $1 RETURNING *';
+  } else if (vote === 'sub') {
+    queryString = 'UPDATE rating_page SET sub_rating = sub_rating + 1 WHERE id = $1 RETURNING *';
+  }
+  console.log(id);
+  await pool.query(
+    queryString,
+    [id],
+  ).then((res) => {
+    result = res;
+  });
+  await pool.end();
+  return result;
+};
 
-module.exports = updateDB;
+app.get('/vote', async (req, res) => {
+  const animeId = req.query.id;
+  const { vote } = req.query;
+
+  const voting = await addVote(vote, animeId);
+  console.log(voting);
+  res.send(voting);
+});
