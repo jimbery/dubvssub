0b9a75bd6b9129a64d5bf075b7753b3dc77ae421
Jay Imbery <jayimbery@hotmail.com>
2025-05-25 12:20:41 +0100

    google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,17 @@ if (rootElement) {
     const root = ReactDOM.createRoot(rootElement)
 
     root.render(
+        
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YC7GN03HQ7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YC7GN03HQ7');
+</script>
+
         <React.StrictMode>
             <Router>
                 <Routes>
